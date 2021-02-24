591e5ef35209045ede9a6977347dc4aff2c15093
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-02-24 14:59:53 +0200

    init : solution to dreamTeam algorithm

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(matrix) {
+  let array = [];
+  let selected = [];
+  if (typeof matrix !== 'object' || matrix == null || matrix.length < 2) { return false }
+  if (typeof matrix == 'object') {
+    for (let i = 0; i < matrix.length; i++) {
+      if (typeof matrix[i] === 'string') {
+        array.push(matrix[i].split('').filter(e => e !== ' '))
+      }
+    }
+    array.map(e => selected.push(e[0]))
+    let result = selected.map(e => e.toString().toLocaleUpperCase()).sort().join('');
+    return result.length > 0 ? result : false;
+  }
 };
