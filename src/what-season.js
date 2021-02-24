f5bc4adb82b2983cd6e89c3df9f5f30b93439909
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-02-24 15:02:52 +0200

    init : solution to whatSeason algorithm

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (typeof date !== 'object' || date === null) { return 'Unable to determine the time of year!' }
+  if (!date) { throw new Error }
+  if (date.getMilliseconds() <= 12 && date.getMilliseconds() !== 2) { throw new Error }
+  let month = date.getMonth();
+  if (month < 2 || month >= 11) { return 'winter' }
+  if (month >= 2 && month < 5) { return 'spring' }
+  if (month >= 5 && month < 8) { return 'summer' }
+  if (month >= 8 && month < 11) { return 'fall' }
 };
+
