9e623bcb208277e04d7081606ef3076a027260a7
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-03-13 14:39:42 +0200

    init : solution to algorithm

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,24 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatArray = [];
+  let repeatFn = (array, string, repeatTimes, separator = '+', addSeparator = '|') => {
+    if (repeatTimes === undefined) {
+      array.push(String(string));
+    } else {
+      for (let i = 0; i < repeatTimes; i++) {
+        array.push(String(string));
+      }
+    }
+    if (separator !== '') {
+      return array.join(separator);
+    } else {
+      return array.join(addSeparator);
+    }
+  };
+  let repeatArrayAdd = [];
+  let strAdd = '';
+  if (options.addition || options.addition === false || options.addition === null) {
+    strAdd = repeatFn(repeatArrayAdd, options.addition, options.additionRepeatTimes, '', options.additionSeparator);
+  }
+  let result = repeatFn(repeatArray, str + strAdd, options.repeatTimes, options.separator, '');
+  return result;
 };
-  
