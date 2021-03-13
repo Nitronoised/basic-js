74e995be28090698d464ab65156e0b8cb6e0b665
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-03-13 14:52:17 +0200

    init : solution to algorithm

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,33 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value !== ' ') {
+      this.chain.push(`( ${value} )`);
+    } else {
+      this.chain.push(`(  )`);
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (Number.isInteger(position || position !== undefined)) {
+      this.chain.splice(position - 1, 1);
+    } else {
+      this.chain = [];
+      throw new Error('its not a number or not exist');
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let stringChain = this.getLength() > 0 ? this.chain.join('~~') : false;
+    this.chain = [];
+    return stringChain;
   }
 };
 
